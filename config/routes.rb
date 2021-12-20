6bc9772eab910ec6c0067774705ecf555de0ac08
Jamie Macey <jamie.git@tracefunc.com>
2021-12-20 22:44:22 +0000

    rails generate administrate:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :users
+      resources :messages
+
+      root to: "users#index"
+    end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
